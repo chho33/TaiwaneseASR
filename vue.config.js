149baf97dc9040d8cd220ad9df57f11be28726e0
jojotenya <jojotenya@gmail.com>
2020-04-25 00:47:47 +0800

    add proxy

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
   devServer: {
       disableHostCheck: true,
-      headers: { 'Access-Control-Allow-Origin': '*' }
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      proxy: 'http://localhost:5000'
   }
 }
